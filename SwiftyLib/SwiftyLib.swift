352552f0f75915c8f88c3f87744c101f1ba8e1bb
Ntobeko Sikithi <ntobekosikithi@gmail.com>
2019-07-05 08:43:01 +0200

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Ntobeko Sikithi on 2019/07/05.
+//  Copyright © 2019 Ntobeko Sikithi. All rights reserved.
+//
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
